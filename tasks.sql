1ac35f879bf8b4f03e84f20e96ee79de61695f13
xavierb <194048107+xavierb117@users.noreply.github.com>
2025-02-27 13:51:32 -0800

    Added code to create database

---
+++ tasks.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE tasks;
+
+USE tasks;
+
+DROP TABLE IF EXISTS submissions;
+
+CREATE TABLE submissions (
+	id INT auto_increment,
+    dateStarted DATE,
+    dateDue DATE,
+    title VARCHAR(255),
+    description VARCHAR(255),
+    priority VARCHAR(255),
+    
+    PRIMARY KEY (id)
+)
